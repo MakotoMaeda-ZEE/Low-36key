410974b6710b4c2e59af316aa47bec1396c207c7
MakotoMaeda <58932183+MakotoMaeda-ZEE@users.noreply.github.com>
2025-08-30 13:10:12 +0900

    コミット１

---
+++ qmk_firmware/keyboards/low_36key/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xc8, 0x10, 0x43, 0x1a, 0x70, 0xe1, 0x70, 0xfe}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
